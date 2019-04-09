11e71e69b00a17774be13c8c04e78fd17e78ceb2
marti <marti@DESKTOP-65875CT>
2019-04-09 21:08:52 +0200

    ja es pot accedir al webservice per https (accepta tots els certificats)

--- app/src/main/java/com/example/marti/projecte_uf1/remote/RetrofitClient.java
+++ app/src/main/java/com/example/marti/projecte_uf1/remote/RetrofitClient.java
@@ -1,5 +1,8 @@
 package com.example.marti.projecte_uf1.remote;
 
+import com.example.marti.projecte_uf1.utils.UnsafeOkHttpClient;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,8 +12,13 @@ public class RetrofitClient {
 
     public static Retrofit getClient(String baseUrl) {
         if (retrofit==null) {
+
+            OkHttpClient okHttpClient = UnsafeOkHttpClient.getUnsafeOkHttpClient();
+
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(baseUrl)
+                    .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
