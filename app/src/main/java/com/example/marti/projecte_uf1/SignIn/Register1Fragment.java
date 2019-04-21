fc85274f66939b22c05cd9c405654dce866c0ad7
marticaixal <mcaix@DESKTOP-8SBI5RE>
2019-04-21 03:20:58 +0200

    cosas del sign in

--- app/src/main/java/com/example/marti/projecte_uf1/SignIn/Register1Fragment.java
+++ app/src/main/java/com/example/marti/projecte_uf1/SignIn/Register1Fragment.java
@@ -1,27 +1,38 @@
 package com.example.marti.projecte_uf1.SignIn;
 
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-;
-import android.content.SharedPreferences;
-
-
+import android.widget.ImageView;
 
 import com.example.marti.projecte_uf1.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
-public class Register1Fragment extends Fragment{
+
+public class Register1Fragment extends Fragment {
 
 
-
+    @BindView(R.id.ivMale)
+    ImageView ivMale;
+    @BindView(R.id.ivFemale)
+    ImageView ivFemale;
+    Unbinder unbinder;
     private String sharedPrefFile = "prefsFile";
     private SharedPreferences prefs;
     private SharedPreferences.Editor prefsEditor;
+    private boolean maleSelected = false;
+    private boolean femaleSelected = false;
+
 
     public Register1Fragment() {
         // Required empty public constructor
@@ -41,10 +52,92 @@ public class Register1Fragment extends Fragment{
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_register1, container, false);
 
+        unbinder = ButterKnife.bind(this, view);
         return view;
     }
 
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    private void increaseMale() {
+        femaleSelected = false;
+        maleSelected = true;
+
+        ObjectAnimator scaleDownX = ObjectAnimator.ofFloat(ivMale, "scaleX", 1.25f);
+        ObjectAnimator scaleDownY = ObjectAnimator.ofFloat(ivMale, "scaleY", 1.25f);
+        scaleDownX.setDuration(1000);
+        scaleDownY.setDuration(1000);
+
+        AnimatorSet scaleDown = new AnimatorSet();
+        scaleDown.play(scaleDownX).with(scaleDownY);
+
+        scaleDown.start();
 
 
+        scaleDownX = ObjectAnimator.ofFloat(ivFemale, "scaleX", .90f);
+        scaleDownY = ObjectAnimator.ofFloat(ivFemale, "scaleY", .90f);
+        scaleDownX.setDuration(1000);
+        scaleDownY.setDuration(1000);
+
+        scaleDown = new AnimatorSet();
+        scaleDown.play(scaleDownX).with(scaleDownY);
+
+        scaleDown.start();
+    }
+
+    private void increaseFemale(){
+        maleSelected = false;
+        femaleSelected = true;
+
+        ObjectAnimator scaleDownX = ObjectAnimator.ofFloat(ivFemale, "scaleX", 1.25f);
+        ObjectAnimator scaleDownY = ObjectAnimator.ofFloat(ivFemale, "scaleY", 1.25f);
+        scaleDownX.setDuration(1000);
+        scaleDownY.setDuration(1000);
+
+        AnimatorSet scaleDown = new AnimatorSet();
+        scaleDown.play(scaleDownX).with(scaleDownY);
+
+        scaleDown.start();
+
+        scaleDownX = ObjectAnimator.ofFloat(ivMale, "scaleX", 0.75f);
+        scaleDownY = ObjectAnimator.ofFloat(ivMale, "scaleY", 0.75f);
+        scaleDownX.setDuration(1000);
+        scaleDownY.setDuration(1000);
+
+        scaleDown = new AnimatorSet();
+        scaleDown.play(scaleDownX).with(scaleDownY);
+
+        scaleDown.start();
+    }
+
+    @OnClick({R.id.ivMale, R.id.ivFemale})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.ivMale:
+                if (!maleSelected) {
+                    increaseMale();
+
+
+                } else {
+                    increaseFemale();
+                }
+
+
+                break;
+            case R.id.ivFemale:
+
+                if (!femaleSelected) {
+                    increaseFemale();
+
+                } else {
+                    increaseMale();
+                }
+
+                break;
+        }
+    }
 }
